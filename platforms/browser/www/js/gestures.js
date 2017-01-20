59bdf4fc101edcc84043968b72879389ad304c63
Jossue Galdamez <jossuegaldamez@iMac-de-Jossue.local>
2017-01-20 14:32:53 -0600

    primer push

---
+++ platforms/browser/www/js/gestures.js
@@ -0,0 +1,50 @@
+var app={
+	inicio:function(){
+		this.iniciaBotones();
+		this.iniciaFastClick();
+		this.iniciarHammer();
+	},
+	iniciaFastClick:function(){
+		FastClick.attach(document.body);
+	},
+	iniciaBotones: function(){
+		var botonClaro = document.querySelector("#claro");
+		var botonOscuro = document.querySelector("#oscuro");
+
+		botonClaro.addEventListener("click", this.ponloClaro,false);
+		botonOscuro.addEventListener("click",this.ponloOscuro,false);
+	},
+	iniciarHammer: function(){
+		var zona = document.getElementById("zona-gestos");
+		var hammertime = new Hammer(zona);
+
+		hammertime.get("pinch").set({enable:true});
+		hammertime.get("rotate").set({enable:true});
+
+		zona.addEventListener("webkitAnimationEnd", function(e){
+			zona.className=" ";
+		});
+
+		hammertime.on("doubletap", function(ev){
+			zona.className="doubletap";
+		});
+
+		hammertime.on("press", function(ev){
+			zona.className="press";
+		});
+	},
+
+	ponloClaro: function(){
+		document.body.className="claro";
+	},
+	ponloOscuro: function(){
+		document.body.className="oscuro";
+	},
+};
+
+if ("addEventListener" in document) {
+	document.addEventListener("DOMContentLoaded", function(){
+		
+		app.inicio();
+	},false)
+}
